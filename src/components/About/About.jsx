da0e1711ab97dddd22054a78866dc4f61b4846e5
SaadSamir7 <saadsamir7177@gmail.com>
2024-11-29 18:46:34 +0200

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,22 @@
 import styles from "./About.module.css";
 
 function About() {
-    return <div className={styles.about}></div>;
+  return (
+    <div className={styles.about} id="about">
+      <div className={styles.overlay}></div>
+      <div className={styles.content}>
+        <h2>About Us</h2>
+        <p>
+          At <span>7erfa</span>, we aim to bridge the gap between customers and
+          skilled workers. Our platform ensures a seamless connection that saves
+          time, enhances reliability, and drives satisfaction. Whether you're a
+          customer looking for assistance or a worker seeking opportunities,{" "}
+          <span>7erfa</span> has you covered.
+        </p>
+        <button>Learn More</button>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,15 @@
+import About from "../components/About/About";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 
 function LandingPage() {
-    return (
-        <>
-            <Header />
-            <Hero />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+    </>
+  );
 }
 
 export default LandingPage;
