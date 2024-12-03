90377119dbf706c12efd37cc1949800bbf04a28d
SalahMSwefy <salahm.swefy@gmali.com>
2024-12-03 13:54:56 +0200

    # React + Vite

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,25 @@
 import styles from "./About.module.css";
 
 function About() {
-  return (
-    <div className={styles.about} id="about">
-      <div className={styles.overlay}></div>
-      <div className={styles.content}>
-        <h2>About Us</h2>
-        <p>
-          At <span>7erfa</span>, we aim to bridge the gap between customers and
-          skilled workers. Our platform ensures a seamless connection that saves
-          time, enhances reliability, and drives satisfaction. Whether you're a
-          customer looking for assistance or a worker seeking opportunities,{" "}
-          <span>7erfa</span> has you covered.
-        </p>
-        <button>Learn More</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.about} id="about">
+            <div className={styles.overlay}></div>
+            <div className={styles.content}>
+                <h2>About Us</h2>
+                <p>
+                    At <span>7erfa</span>, we aim to bridge the gap between
+                    customers and skilled workers. Our platform ensures a
+                    seamless connection that saves time, enhances reliability,
+                    and drives satisfaction. Whether you're a customer looking
+                    for assistance or a worker seeking opportunities,{" "}
+                    <span>7erfa</span> has you covered.
+                </p>
+                <button>
+                    <a href="#services">Learn More</a>
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default About;
