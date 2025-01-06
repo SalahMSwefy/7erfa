5a3d7996e8f2fcfd17353183ef7004454cd6e140
SalahMSwefy <salahm.swefy@gmali.com>
2025-01-06 22:08:42 +0200

    # React + Vite

--- src/context/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom'
-
-const PrivateRoute = ({ children, isAuthenticated }) => {
-    return isAuthenticated ? children : <Navigate to="/login" replace />
-}
-
-export default PrivateRoute
