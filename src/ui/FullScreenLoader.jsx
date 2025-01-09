9f237f0d66873f3fc1858f651bba7c384fcb9931
SalahMSwefy <salahm.swefy@gmali.com>
2025-01-09 03:06:28 +0200

    # React + Vite

--- src/ui/FullScreenLoader.jsx
+++ src/ui/FullScreenLoader.jsx
@@ -1,6 +1,6 @@
 function FullScreenLoader() {
     return (
-        <div className="flex min-h-screen items-center justify-center">
+        <div className="flex min-h-screen items-center justify-center bg-white dark:bg-gray-500">
             <img src="/logos/logo.gif" alt="Loading..." className="h-36 w-36" />
         </div>
     )
